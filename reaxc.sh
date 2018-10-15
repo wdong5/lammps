989cc5d951fd3afa0aa395568b1adfa2b632d6a2
wdong5 <38866689+wdong5@users.noreply.github.com>
2018-10-14 20:42:58 -0700

    Add files via upload

--- reaxc.sh
+++ reaxc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a comment!
-echo Hello World        # This is a comment, too!
 
-$git pull origin master
-$cd build
-$rm -rf *
-$cmake ../cmake -DPKG_TF=YES
-$make 
-$cd ../examples/wdong_reaxc/
-$../../build/lmp < in.reaxc
+cd build/
+rm -rf *
+git pull origin master
+cmake ../cmake -DPKG_TF=YES
+make 
+cd ../examples/wdong_reaxc/
+../../build/lmp < in.reaxc
+
