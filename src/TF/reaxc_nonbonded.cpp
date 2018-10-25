4d5356449bd397fafe19da08461f8f8f6b754935
wdong5 <38866689+wdong5@users.noreply.github.com>
2018-10-25 11:34:21 -0700

    Update reaxc_nonbonded.cpp

--- src/TF/reaxc_nonbonded.cpp
+++ src/TF/reaxc_nonbonded.cpp
@@ -280,7 +280,7 @@ void vdW_Coulomb_Energy( reax_system *system, control_params *control,
         }
 		gettimeofday( &end_bp8, NULL );
 		bp8 = bp8 + 1000000 * (end_bp8.tv_sec - start_bp8.tv_sec) + end_bp8.tv_usec - start_bp8.tv_usec;
-		//std::cout<<"bp8 time:"<<bp8<<" \n";
+		std::cout<<"bp8 time:"<<bp8<<" \n";
 
      
 
